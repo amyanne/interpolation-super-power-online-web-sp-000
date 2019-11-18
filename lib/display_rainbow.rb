5bad48a9ea0020401265cb4001927873dafcc624
Amy Westcott <amy.westcott@yahoo.com>
2019-11-18 18:37:21 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 # Write your #display_rainbow method here
 def display_rainbow(rainbow)
-  puts "R: #{rainbow[0]}, O: "
+  puts "R: #{rainbow[0]}, O: #{rainbow[1]}, Y: #{rainbow[2]}, G: #{rainbow[3]}, B: #{rainbow[4]}, I: #{rainbow[5]}, #{rainbow[6]} "
   
 end 
